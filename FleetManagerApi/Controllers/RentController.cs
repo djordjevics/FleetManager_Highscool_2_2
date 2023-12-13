e730eadf7bcc78852a843051a022a16f0b375383
unagagovic <gagovicuna@gmail.com>
2023-12-13 13:31:40 +0100

    Added endpoints GetAll, GetByID and Create in RentController

--- FleetManagerApi/Controllers/RentController.cs
+++ FleetManagerApi/Controllers/RentController.cs
@@ -7,5 +7,32 @@ namespace FleetManagerApi.Controllers
     [ApiController]
     public class RentController : ControllerBase
     {
+        [HttpGet("GetAll")]
+        [ProducesResponseType(200)]
+       
+        public IActionResult GetAll()
+        {
+            return Ok(new int[] { 1, 2, 3 });
+        }
+        
+        [HttpGet("GetByID/{ID}")]
+        [ProducesResponseType(200)]
+        
+        public IActionResult Get(int id)
+        {
+            return Ok(id);
+        }
+
+        [HttpPost("Create")]
+        [ProducesResponseType(200)]
+
+        public IActionResult Create(Temp obj)
+        {
+            obj.id += 3;
+            obj.name += "asd";
+            return Ok(obj);
+        }
     }
 }
+
+public class Temp { public int id { get; set; } public string name { get; set; } }
