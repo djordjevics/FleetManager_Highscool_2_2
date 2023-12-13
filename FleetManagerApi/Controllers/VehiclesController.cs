c966703230cd1fa1b0bdd12157bfa5648c176398
UgljesaJovina <starcevic.ugljesa@gmail.com>
2023-12-13 12:57:41 +0100

    GetAll Vehicles

--- FleetManagerApi/Controllers/VehiclesController.cs
+++ FleetManagerApi/Controllers/VehiclesController.cs
@@ -6,6 +6,9 @@ namespace FleetManagerApi.Controllers;
 [ApiController]
 public class VehiclesController : ControllerBase
 {
+    [HttpGet("GetAll")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(400, Type = typeof(string))] // if we have an error, we return a message containing the error reason
     public IActionResult GetAll()
     {
         return Ok(new int[]{ 1, 2, 3 });
