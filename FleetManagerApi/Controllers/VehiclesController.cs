782977ce81adc88d5e3c82a294dd399e2e390111
unnccciiii <egelja.una@jjzmaj.edu.rs>
2023-12-13 13:16:01 +0100

    Added endpoints to vehicle controller

--- FleetManagerApi/Controllers/VehiclesController.cs
+++ FleetManagerApi/Controllers/VehiclesController.cs
@@ -11,6 +11,23 @@ public class VehiclesController : ControllerBase
     [ProducesResponseType(400, Type = typeof(string))] // if we have an error, we return a message containing the error reason
     public IActionResult GetAll()
     {
-        return Ok(new int[]{ 1, 2, 3 });
-    } 
+        return Ok(new int[] { 1, 2, 3 });
+    }
+
+    [HttpGet("GetById/{id}")]
+    [ProducesResponseType(200)]
+    public IActionResult GetById(int id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPost("Create")]
+    [ProducesResponseType(200)]
+    public IActionResult Create(Temp obj)
+    {
+        obj.id += 3;
+        obj.name += "asd";
+        return Ok(obj);
+    }
 }
+public class Temp { public int id { get; set; } public string name { get; set; } }
