38edea31cf7f7538bccdbb5ac6cbf2a37a7f0ee6
UgljesaJovina <starcevic.ugljesa@gmail.com>
2023-12-15 13:37:34 +0100

    added fuellog

---
+++ Repositories/Models/FuelLog.cs
@@ -0,0 +1,18 @@
+﻿namespace Repositories.Models
+{
+    public class FuelLog
+    {
+        public int Id { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public double Consumption { get; set; }
+
+        public FuelLog() { }
+
+        public FuelLog(int id, Vehicle vehicle, double consumption)
+        {
+            Id = id;
+            Vehicle = vehicle;
+            Consumption = consumption;
+        }
+    }
+}
